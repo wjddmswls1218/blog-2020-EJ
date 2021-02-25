746d25e414b9458900fa3f3cefdbdd74eb582e71
Eun Jin <ouhouh2222@gmail.com>
2021-02-26 00:42:19 +0900

    Post data

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,12 +2,47 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const Post = new Schema({
-  title: {
-    type: String,
-    reqired: true,
+const Post = new Schema(
+  {
+    title: {
+      type: String,
+      reqired: true,
+    },
+    description: {
+      type: String,
+      reqired: true,
+    },
+    author: {
+      type: String,
+      reqired: true,
+    },
+    hit: {
+      type: Number,
+      reqired: true,
+    },
+    PostType: {
+      type: mongoose.Types.ObjectId,
+      ref: `PostType`,
+    },
+    createdAt: {
+      type: String,
+      required: true,
+    },
+    lastUpdatedAt: {
+      type: String,
+      required: true,
+    },
+    isDelete: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    no: {
+      type: Number,
+      required: true,
+    },
   },
-  description: {
-    type,
-  },
-});
+  { versionKey: false }
+);
+
+export default mongoose.model(`Post`, Post, `Post`);
